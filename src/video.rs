1fc69aea03ed71b8bddb9ffebb7d08ff67fcf8c9
lisiurday <lisiurday@gmail.com>
2021-08-22 10:48:32 +0800

    fix: Fix log

--- src/video.rs
+++ src/video.rs
@@ -19,12 +19,12 @@ pub async fn download_video(page: &dyn VideoDetailPage) -> AppResult<()> {
     let video = page.extract_video().await?;
     if let Some(video) = video {
         let video_name = format!("【{}】{}.mp4", &video.author, &video.title);
-        log::info!("【下载中...】 {}", video_name);
+        log::info!("【下载中...】");
         let client = reqwest::Client::builder()
             .timeout(Duration::from_secs(10))
             .build()?;
         let resp = client.get(&video.src).send().await?;
-        log::info!("【下载成功】");
+        log::info!("【下载成功】 {}", video_name);
         let bytes = resp.bytes().await?;
         let mut slice: &[u8] = bytes.as_ref();
         let mut out = File::create(&video_name)?;
